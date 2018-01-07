f7a67d548ddb5a940f27960ee39d7bc1b7ffc8bf
Chris K <chris.koeberle@bottlerocketstudios.com>
2018-01-07 07:59:44 -0600

    Create the manifest

--- src/main/java/invision/HelloWorld.kt
+++ src/main/java/invision/HelloWorld.kt
@@ -1,10 +1,21 @@
 package invision
 
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.squareup.kotlinpoet.*
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import java.io.File
 import java.util.regex.Pattern
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.dom.DOMSource
+import com.sun.xml.internal.ws.addressing.EndpointReferenceUtil.transform
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.Transformer
+import javax.xml.transform.TransformerFactory
+
+
 
 fun main(args: Array<String>) {
     val invisionJson = getStringFromFile(s=File(args[0]).absoluteFile.toString() + "/index_pretty.json")
@@ -14,17 +25,25 @@ fun main(args: Array<String>) {
     val invision = adapter.fromJson(invisionJson)
     invision?:return
 
-    val buildGradle = getStringFromFile(s=File(args[1]).absoluteFile.toString()+"/app/build.gradle")
+    val path = File(args[1]).absoluteFile.toString()
+    val buildGradle = getStringFromFile(s=path+"/app/build.gradle")
     val appIdPattern = Pattern.compile("""applicationId\s*(["'`])([^"'`]*)\1""")
     val appIdMatcher = appIdPattern.matcher(buildGradle)
     if (appIdMatcher.find()) else return
     val packageName = appIdMatcher.group(2)
     val invisionPackage = packageName + ".invision"
+    val dbf = DocumentBuilderFactory.newInstance()
+    val db = dbf.newDocumentBuilder()
+    val manifest = db.parse(File(path + "/app/src/main/AndroidManifest.xml"))
+    val activities = manifest.getElementsByTagName("activity")
+    val application = manifest.getElementsByTagName("application").item(0)
     System.out.println(packageName)
     System.out.println("There are ${invision.hotspots.size} hotspots on ${invision.screens.size} screens!")
     invision.screens.forEach {
         val className = it.name.replace(Regex("^.*B2B "), "").replace(" ", "") + "Activity"
+        val viewClassName = ClassName("android.widget", "View")
         val bundleClassName = ClassName("android.os", "Bundle")
+        val imageViewClassName = ClassName("android.widget", "ImageView")
         System.out.println(className)
         val screenClassName = ClassName(invisionPackage, className)
         val baseClassName = ClassName(invisionPackage, "BaseInvisionActivity")
@@ -32,20 +51,43 @@ fun main(args: Array<String>) {
 
         val screenClass = TypeSpec.classBuilder(className)
                 .superclass(baseClassName)
+                .addProperty(PropertySpec.builder("wire", imageViewClassName).mutable(true).addModifiers(KModifier.LATEINIT).build())
                 .addFunction(FunSpec.builder("onCreate")
                         .addParameter("savedInstanceState", bundleClassName.asNullable())
                         .addModifiers(KModifier.OVERRIDE)
-                        .addStatement("super.onCreate(%L)", "savedInstanceState")
+                        .addStatement("super.onCreate(savedInstanceState)", "savedInstanceState")
                         .addStatement("setContentView(%L)", "R.layout.invision_activity")
-                        .addStatement("")
+                        .addStatement("%L=findViewById(%L)", "wire", "R.id.wire")
+                        .beginControlFlow("%L.setOnTouchListener", "wire")
+                        .addStatement("view, motionEvent -> ")
+                        .beginControlFlow("%L.forEach","hotspots")
+                        .beginControlFlow("if(it.contains(view, motionEvent))")
+                        .addStatement("val i = Intent(this, it.target)")
+                        .addStatement("startActivity(i)")
+                        .endControlFlow()
+                        .endControlFlow()
                         .build())
                 .build()
-        val file = FileSpec.builder(invisionPackage, className)
+        val fileSpec = FileSpec.builder(invisionPackage, className)
                 .addType(screenClass)
                 .addStaticImport(packageName, "R")
                 .build()
-        file.writeTo(System.out)
-
+        val output = File(path + "/app/src/main/java/")
+        fileSpec.writeTo(output)
+        if (!(0 .. activities.length-1).any {
+            activities.item(it).attributes.getNamedItem("android:name").nodeValue.contains(className)?:false
+        }) {
+            val newActivity = manifest.createElement("activity")
+            newActivity.setAttribute("android:name", ".invision."+className)
+            application.appendChild(newActivity)
+        }
+        val ds = DOMSource(manifest)
+        val transformerFactory = TransformerFactory.newInstance()
+        val transformer = transformerFactory.newTransformer()
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+        val result = StreamResult(path + "/app/src/main/NewAndroidManifest.xml")
+        transformer.transform(ds, result)
     }
 /*
     val greeterClass = ClassName("", args[0])
@@ -86,3 +128,5 @@ data class Hotspot (val height: Int, val width: Int, val x: Int, val y: Int, val
 data class Project (val homeScreenId: Int)
 
 data class Screen (val imageUrl: String, val height: Int, val width: Int, val id: Int, val name: String)
+
+
